1bf3b9950dec57bfbb0bd18cce517999f58fd964
Wesley Farias <68573544+wesleyfariias@users.noreply.github.com>
2022-09-05 12:36:13 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,20 +1,20 @@
 ### GCP
-###variable "gcp_project" {
+variable "gcp_project" {
   type        = string
   description = "Projeto do GCP"
-}###
+}
 
-###variable "gcp_region" {
+variable "gcp_region" {
   type        = string
   description = "Região do GCP"
   default     = "us-central1"
-}###
+}
 
-###variable "gcp_zone" {
+variable "gcp_zone" {
   type        = string
   description = "Zona no GCP"
   default     = "us-central1-c"
-}###
+}
 ### AZURE
 
 variable "rg_location" {
@@ -25,4 +25,29 @@ variable "rg_location" {
 variable "azurevm_admin_pass" {
   type        = string
   description = "Senha Usuario Admin do Linux"
+}
+### CLOUDFLARE
+
+#Cloudflare email
+variable "cloudflare_email" {
+  type = string
+  description = "Cloudflare Email Address"
+}
+
+#Cloudflare API chave
+variable "cloudflare_api_key" {
+  type = string
+  description = "Cloudflare API Key"
+}
+
+#Cloudflare zone id
+variable "cloudflare_zone_id" {
+  type = string
+  description = "Cloudflare Zone ID"
+}
+
+#Cloudflare Record Name
+variable "cloudflare_record" {
+  type = string
+  description = "Cloudflare Record Name"
 }
